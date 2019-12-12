e94e76aec96244d281b31882612e43f20c68d318
itamar-mizrahi <theitamarmizrahi@gmail.com>
2019-12-12 19:52:08 +0200

    feat back button

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Card from '@material-ui/core/Card';
 import AppleIcon from '@material-ui/icons/Apple';
+import { Button } from "@material-ui/core";
 
 
 class Song extends Component {
@@ -17,14 +18,17 @@ class Song extends Component {
         <h1>{this.state.item.collectionName}</h1>
         <AppleIcon fontSize="large"/>
         {this.state.item.previewUrl?  (
-          <Card className="card-row">
+          <Card className="card-column">
           <iframe
             title="itunes"
             src={this.state.item.previewUrl}
             height="600"
             width="1400"
           ></iframe>
+          <Button variant="contained" color="primary" onClick={e =>
+            this.props.history.push(`/`)}>back</Button>
           </Card>
+          
         ) : (
           "No prevURL found"
         )}
