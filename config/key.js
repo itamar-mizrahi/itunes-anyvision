cd2c982f5426cd763c3b8860ec4f56bf80a3da19
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2023-04-17 01:45:05 +0300

    key

--- config/key.js
+++ config/key.js
@@ -1,3 +1,5 @@
-const key='mongodb+srv://theitamarmizrahi:3519902786a@cluster0-iapps.mongodb.net/itunes?retryWrites=true&w=majority'
+const key='mongodb+srv://theitamarmizrahi:password@cluster0-iapps.mongodb.net/itunes?retryWrites=true&w=majority'
 
 module.exports=key
+
+//password:...
